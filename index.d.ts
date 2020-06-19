02ef0d3d608376c0046b7ea8cd9ee334327ee0c8
Mark Herhold <mark.herhold@gmail.com>
2020-06-18 20:13:14 -0400

    adding the transaction interface

--- index.d.ts
+++ index.d.ts
@@ -1,11 +1,10 @@
 declare module 'data-api-client' {
-    import type RDSDataService from 'aws-sdk/clients/rdsdataservice';
     import type { ClientConfiguration } from 'aws-sdk/clients/rdsdataservice';
 
     export interface iParams {
         secretArn: string;
         resourceArn: string;
-        database: string;
+        database?: string;
         keepAlive?: boolean;
         hydrateColumnNames?: boolean;
         sslEnabled?: boolean;
@@ -13,8 +12,28 @@ declare module 'data-api-client' {
         region?: string;
     }
 
+    interface iTransaction {
+        query: iQuery<iTransaction>;
+        rollback: (error: Error, status: any) => void;
+        commit: () => Promise<void>;
+    }
+
+    interface iQuery<T> {
+        (sql: string, params?: [] | unknown): Promise<T>; // params can be [] or {}
+        (obj: { sql: string; parameters: [] | unknown; database: string; hydrateColumnNames?: boolean }): Promise<T>;
+    }
+
     export interface iDataAPIClient {
-        query(...x: any[]): Promise<iDataAPIQueryResult>;
+        query: iQuery<iDataAPIQueryResult>;
+        transaction(): iTransaction; // needs to return an interface with
+
+        // promisified versions of the RDSDataService methods
+        // TODO add actual return types
+        batchExecuteStatement: (...args) => Promise<any>;
+        beginTransaction: (...args) => Promise<any>;
+        commitTransaction: (...args) => Promise<any>;
+        executeStatement: (...args) => Promise<any>;
+        rollbackTransaction: (...args) => Promise<any>;
     }
 
     export interface iDataAPIQueryResult {
@@ -23,4 +42,3 @@ declare module 'data-api-client' {
 
     export default function (params: iParams): iDataAPIClient;
 }
-
