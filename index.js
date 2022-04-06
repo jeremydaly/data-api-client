de431fa2609e3098eada9b65d2b6a3183fa22651
Jeremy Daly <jeremydaly@users.noreply.github.com>
2022-04-06 14:54:00 -0400

    version bump

--- index.js
+++ index.js
@@ -8,7 +8,7 @@
  * https://docs.aws.amazon.com/AmazonRDS/latest/AuroraUserGuide/data-api.html
  *
  * @author Jeremy Daly <jeremy@jeremydaly.com>
- * @version 1.2.0
+ * @version 2.0.0-beta
  * @license MIT
  */
 
